4dfa42c92e3bffa3b8d3d0d7f1e78a4082ddba65
Lawrence Beck <sonny1388@msn.com>
2018-09-05 21:26:51 -0600

    Changed files to be Heroku deploy ready

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = props => (
+	<div>
+	<h1>
+	Welcome to my home page!
+	</h1>
+	<p>Thanks for visiting. This site is being built from scratch using a MERN stack. 
+	If you come back from time to to, you will get to watch as this site goes from this
+	plain, boring, vanilla to something that is much more pleasing to the user.
+	</p>
+	</div>
+	)
+
+
+
+export default App
